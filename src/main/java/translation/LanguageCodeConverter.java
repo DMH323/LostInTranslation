6cf8e854d1f824144e95f7dabd856c8f5f012b08
Sushaan Patel <sushaanpatel@gmail.com>
2025-09-22 10:06:18 -0400

    Completed task A

--- src/main/java/translation/LanguageCodeConverter.java
+++ src/main/java/translation/LanguageCodeConverter.java
@@ -42,7 +42,12 @@ public class LanguageCodeConverter {
             iterator.next(); // skip the first line
             while (iterator.hasNext()) {
                 String line = iterator.next();
-                // TODO Task A: use line to populate the instance variables
+                String[] parts = line.split("\t");
+                if (parts.length >= 2) {
+                    languageCodeToLanguage.put(parts[1], parts[0]);
+                    languageToLanguageCode.put(parts[0], parts[1]);
+                }
+
             }
 
         } catch (IOException | URISyntaxException ex) {
@@ -56,8 +61,7 @@ public class LanguageCodeConverter {
      * @return the name of the language corresponding to the code
      */
     public String fromLanguageCode(String code) {
-        // TODO Task A: update this code to use the correct instance variable to return the appropriate value
-        return code;
+        return languageCodeToLanguage.get(code);
     }
 
     /**
@@ -66,8 +70,7 @@ public class LanguageCodeConverter {
      * @return the 2-letter code of the language
      */
     public String fromLanguage(String language) {
-        // TODO Task A: update this code to use the correct instance variable to return the appropriate value
-        return language;
+        return languageToLanguageCode.get(language);
     }
 
     /**
@@ -76,5 +79,12 @@ public class LanguageCodeConverter {
      */
     public int getNumLanguages() {
         return languageCodeToLanguage.size();
+    }
+
+    public static void main(String[] args) {
+        LanguageCodeConverter converter = new LanguageCodeConverter();
+        System.out.println(converter.fromLanguageCode("en")); // should print "English"
+        System.out.println(converter.fromLanguage("French")); // should print "fr"
+        System.out.println(converter.getNumLanguages()); // should print the number of languages loaded
     }
 }
